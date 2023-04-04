329fa5e0ef5646fb07bd9d8c76e8114c72a635ec
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-04-04 15:17:20 +0500

    still setting up

---
+++ src/configs/app.config.ts
@@ -0,0 +1,96 @@
+import { config } from 'dotenv';
+config();
+
+const AppConfig = {
+  APP: {
+    NAME: 'API',
+    PORT: Number(process.env.APP_PORT),
+    DEBUG: Boolean(process.env.APP_DEBUG),
+    LOG_LEVEL: Number(process.env.APP_LOG_LEVEL),
+    TOKEN_EXPIRATION: Number(process.env.APP_TOKEN_EXPIRATION),
+    REMEMBER_TOKEN:
+      Number(process.env.APP_REMEMBER_TOKEN_EXPIRATION) ?? 7776000,
+  },
+  DATABASE: {
+    URL: process.env.APP_DATABASE_URL,
+  },
+  REDIS: {
+    HOST: process.env.APP_REDIS_HOST,
+    PORT: Number(process.env.APP_REDIS_PORT),
+  },
+  AWS: {
+    ACCESS_KEY: process.env.APP_AWS_ACCESS_KEY,
+    SECRET_KEY: process.env.APP_AWS_SECRET_KEY,
+    REGION: process.env.APP_AWS_REGION,
+    BUCKET: process.env.APP_AWS_BUCKET,
+    BUCKET_BASE_URL: process.env.APP_AWS_BUCKET_BASE_URL,
+    STS_ROLE_ARN: process.env.APP_AWS_STS_ROLE_ARN,
+    QUEUE_URL: process.env.APP_AWS_QUEUE_URL,
+    SES_FROM_EMAIL: process.env.APP_AWS_SES_FROM_EMAIL,
+  },
+  TWILIO: {
+    ACCOUNT_SID: process.env.APP_TWILIO_ACCOUNT_SID,
+    AUTH_TOKEN: process.env.APP_TWILIO_AUTH_TOKEN,
+    FROM_NUMBER: process.env.APP_TWILIO_FROM_NUMBER,
+  },
+  OAUTH: {
+    GOOGLE: process.env.APP_GOOGLE_OAUTH_ENDPOINT,
+    APPLE: process.env.APP_APPLE_OAUTH_ENDPOINT,
+    FACEBOOK: process.env.APP_FACEBOOK_OAUTH_ENDPOINT,
+  },
+  NOTIFICATION: {
+    GOOGLE: {
+      FIREBASE_PROJECT_ID: process.env.APP_FIREBASE_PROJECT_ID,
+      FIREBASE_PRIVATE_KEY: process.env.APP_FIREBASE_PRIVATE_KEY.replace(
+        /\\n/gm,
+        '\n',
+      ),
+      FIREBASE_CLIENT_EMAIL: process.env.APP_FIREBASE_CLIENT_EMAIL,
+    },
+  },
+  E_SIGNATURE: {
+    DOCUSIGN_ACCOUNT_ID: process.env.APP_DOCUSIGN_ACCOUNT_ID,
+    DOCUSIGN_OAUTH_BASE_PATH: process.env.APP_DOCUSIGN_OAUTH_BASE_URL,
+    DOCUSIGN_OAUTH_CALLBACK_PATH: process.env.APP_DOCUSIGN_OAUTH_CALLBACK_URL,
+    DOCUSIGN_BASE_PATH: process.env.APP_DOCUSIGN_BASE_URL,
+    DOCUSIGN_INTEGRATION_KEY: process.env.APP_DOCUSIGN_INTEGRATION_KEY,
+    DOCUSIGN_SECRET_KEY: process.env.APP_DOCUSIGN_SECRET_KEY,
+    DOCUSIGN_WEB_RETURN_URL: process.env.APP_DOCUSIGN_WEB_RETURN_URL,
+    DOCUSIGN_RETURN_URL: process.env.APP_DOCUSIGN_RETURN_URL,
+    DOCUSIGN_WEBHOOK_SECRET: process.env.APP_DOCUSIGN_WEBHOOK_SECRET,
+  },
+  IN_APP_PURCHASE: {
+    GOOGLE: {
+      IAP_GOOGLE_EMAIL: process.env.APP_IAP_GOOGLE_EMAIL,
+      IAP_GOOGLE_SECRET_KEY: process.env.APP_IAP_GOOGLE_SECRET_KEY.replace(
+        /\\n/gm,
+        '\n',
+      ),
+      IAP_ANDROID_PACKAGE_NAME: process.env.APP_IAP_ANDROID_PACKAGE_NAME,
+    },
+    APPLE: {
+      IAP_APPLE_SECRET_KEY: process.env.APP_IAP_APPLE_SECRET_KEY.replace(
+        /\\n/gm,
+        '\n',
+      ),
+    },
+  },
+  PAYMENT: {
+    STRIPE: {
+      SECRET_KEY: process.env.APP_STRIPE_SECRET_KEY,
+      CURRENCY: process.env.APP_STRIPE_CURRENCY,
+      WEBHOOK: process.env.APP_STRIPE_WEBHOOK_KEY,
+    },
+    PAYPAL: {
+      CLIENT_ID: process.env.APP_PAYPAL_CLIENT_ID,
+      CLIENT_SECRET: process.env.APP_PAYPAL_CLIENT_SECRET,
+      MODE: process.env.APP_PAYPAL_ENVIRONMENT,
+      WEBHOOK_ID: process.env.APP_PAYPAL_WEBHOOK_ID,
+      RETURN_URL: process.env.APP_PAYPAL_RETURN_URL,
+      CANCEL_URL: process.env.APP_PAYPAL_CANCEL_URL,
+    },
+  },
+  CHAT_SESSION: Number(process.env.APP_CHAT_SESSION_EXPIRATION),
+};
+
+export default AppConfig;
---
+++ src/helpers/date.helper.ts
@@ -0,0 +1,313 @@
+// import * as dayjs from 'dayjs-timezone';
+import dayjs from 'dayjs';
+
+export enum AllowDateFormat {
+  ISODate = 'YYYY-MM-DD',
+  YearMonth = 'YYYY-MM',
+  ShortDate = 'MM/DD/YYYY',
+  LongDate = 'MMM DD YYYY',
+  StringDate = 'YYYY-MM-DDTHH:mm:ss.sssZ',
+  DateTime = 'YYYY-MM-DD HH:mm:ss',
+  TimeWithOutSeconds = 'LT',
+  TimeWithSeconds = 'LTS',
+  DD = 'DD',
+  MM = 'MM',
+  YYYY = 'YYYY',
+  YearWeek = 'GGGG-WW',
+  Time24HourFormat = 'HH:mm',
+  FullMonth = 'MMMM',
+  UnixTimeStamp = 'x',
+  WeekDay = 'dddd',
+}
+
+export function Now() {
+  return dayjs().toDate();
+}
+
+// export function ConvertDurationToSeconds(duration, unit: dayjs.DurationInputArg2 = 'minutes') {
+//     return dayjs.duration(duration, unit).asSeconds();
+// }
+
+export function ToTimeStamp(date) {
+  return dayjs(date).valueOf();
+}
+
+export function IsDateFormatValid(date, format) {
+  return dayjs(date, format, true).isValid();
+}
+
+export function GetCurrentWeekDay() {
+  return dayjs().format('dddd');
+}
+
+export function ConvertToDate(dateString: string, format: AllowDateFormat) {
+  return dayjs(dateString, format).toDate();
+}
+
+// export function IsSameOrAfter(firstDate, secondDate) {
+//     return dayjs(firstDate).isSameOrAfter(secondDate);
+// }
+
+// export function IsSameOrBefore(firstDate, secondDate) {
+//     return dayjs(firstDate).isSameOrBefore(secondDate);
+// }
+
+export function IsSame(firstDate, secondDate) {
+  return dayjs(firstDate).isSame(secondDate);
+}
+
+export function IsBefore(firstDate, secondDate) {
+  return dayjs(firstDate).isBefore(secondDate);
+}
+
+export function ConvertToSpecificFormat(date: Date, format: AllowDateFormat) {
+  return dayjs(date).format(format);
+}
+
+export function SubtractDays(date, days) {
+  return dayjs(date).subtract(days, 'days').toDate();
+}
+
+export function AddMinutes(date, minutes: number) {
+  return dayjs(date).add(minutes, 'minutes').toDate();
+}
+
+export function AddDays(date, days) {
+  return dayjs(date).add(days, 'days').toDate();
+}
+
+export function AddMonths(date, month) {
+  return dayjs(date).add(month, 'month').toDate();
+}
+
+export function SubtractMonths(date, month) {
+  return dayjs(date).subtract(month, 'month').toDate();
+}
+
+export function AddHours(date, hours) {
+  return dayjs(date).add(hours, 'hour').toDate();
+}
+
+export function SubtractHours(date, hour) {
+  return dayjs(date).subtract(24, 'hour').toDate();
+}
+
+export function SubtractYears(date, years) {
+  return dayjs(date).subtract(years, 'year').toDate();
+}
+
+export function DiffBetweenTwoDates(firstDate, secondDate) {
+  return dayjs(secondDate).diff(dayjs(firstDate), 'days');
+}
+
+export function DiffBetweenTwoDatesInMinutes(firstDate, secondDate) {
+  return dayjs(secondDate).diff(dayjs(firstDate), 'minutes');
+}
+
+export function StartDateOfMonth(date) {
+  return dayjs(date).startOf('month').toDate();
+}
+
+export function EndDateOfMonth(date) {
+  return dayjs(date).endOf('month').toDate();
+}
+
+export function StartOfDay(date) {
+  return dayjs(date).startOf('day').toDate();
+}
+
+export function EndOfDay(date) {
+  return dayjs(date).endOf('day').toDate();
+}
+
+export function StartOfYear(date) {
+  return dayjs(date).startOf('year').toDate();
+}
+
+export function EndOfYear(date) {
+  return dayjs(date).endOf('year').toDate();
+}
+
+export function GetDate(date: Date) {
+  return dayjs(date).date();
+}
+
+export function GetMonth(date: Date) {
+  return dayjs(date).month();
+}
+
+export function GetYear(date: Date) {
+  return dayjs(date).year();
+}
+
+// export function GetMomentFromString(dateString, format: AllowDateFormat, tz = 'UTC') {
+//     return dayjs.tz(dateString, format, tz);
+// }
+
+// export function GetMomentFromTimezone(date: Date, tz) {
+//     return dayjs.tz(date, tz);
+// }
+
+// export function GetTimezoneDate(date: Date, tz) {
+//     return dayjs.tz(date, tz).toDate();
+// }
+
+export function GetLastDayOfYear(year) {
+  return dayjs(year, 'YYYY').endOf('year').toDate();
+}
+
+export function IsFuture(date: Date) {
+  return dayjs(date).isAfter(dayjs());
+}
+
+// export function getSeries(
+//     startDate: dayjs.Moment,
+//     endDate: dayjs.Moment,
+//     type: dayjs.unitOfTime.DurationConstructor,
+// ): string[] {
+//     if (startDate.isSameOrAfter(endDate)) {
+//         throw new Error('Invalid date range supplied');
+//     }
+
+//     let series: string[] = [];
+//     let format: string = null;
+
+//     switch (type) {
+//         case 'year':
+//             format = AllowDateFormat.YYYY;
+//             break;
+//         case 'month':
+//             format = AllowDateFormat.YYYY + '-' + AllowDateFormat.MM;
+//             break;
+//         case 'week':
+//             format = 'gggg-ww';
+//             break;
+//         default:
+//             type = 'day';
+//             format = AllowDateFormat.YYYY + '-' + AllowDateFormat.MM + '-' + AllowDateFormat.DD;
+//     }
+
+//     while (true) {
+//         series.push(startDate.format(format));
+//         startDate = startDate.add(1, type as dayjs.unitOfTime.DurationConstructor);
+//         if (startDate.isAfter(endDate)) {
+//             break;
+//         }
+//     }
+//     return series;
+// }
+
+// export function getYearKeys(year) {
+//     let startDate = dayjs(year, AllowDateFormat.YYYY).startOf('year');
+//     let endDate = startDate.clone().endOf('year');
+//     let keys: string[] = getSeries(startDate, endDate, 'month');
+//     return keys;
+// }
+
+// export function generateLastXMonthKeys(today: dayjs.Moment, Months: number) {
+//     let endDate = today;
+//     let startDate = today.clone().subtract(Months - 1, 'months');
+//     let keys: string[] = getSeries(startDate, endDate, 'month');
+//     return keys;
+// }
+
+// export function generateWeekIntervalKeysBetweenTwoDates(startDate: Date, endDate: Date) {
+//     return getSeries(dayjs(startDate), dayjs(endDate), 'week');
+// }
+
+// export function generateDayIntervalKeys(endDate: dayjs.Moment, interval) {
+//     let startDate = endDate.clone().subtract(interval - 1, 'days');
+//     let keys: string[] = getSeries(startDate, endDate, 'day');
+//     return keys;
+// }
+
+// export function generateDayIntervalKeysBetweenTwoDates(startDate: Date, endDate: Date) {
+//     return getSeries(dayjs(startDate), dayjs(endDate), 'days');
+// }
+export function addMinutes(date, minutes) {
+  return new Date(date.getTime() + minutes * 60000);
+}
+/**
+ * @function
+ * @description returns range (start & end datetime) of previous Quarter of the hour or the datetime provided
+ */
+// export function getPreviousQuarterHourRange(dateTime = dayjs()) {
+//   if (typeof dateTime === 'string') {
+//     dateTime = dayjs(dateTime, AllowDateFormat.DateTime);
+//   }
+
+//   let startTime = dateTime.clone().subtract(15, 'minutes');
+//   startTime.minutes(Math.floor(startTime.minutes() / 15) * 15);
+//   startTime.seconds(0);
+
+//   let endTime = startTime.clone();
+//   endTime.add(15, 'minutes').subtract(1, 'second');
+
+//   return {
+//     start: startTime,
+//     end: endTime,
+//   };
+// }
+
+/**
+ * @function
+ * @description Get available timezones
+ * @returns {string[]}
+ */
+// export function getAvailableTimezones() {
+//     return dayjs.tz.names().filter((tz) => {
+//         let tzl = tz.toLowerCase();
+//         return tzl.indexOf('etc/') === -1 && tzl.indexOf('/') !== -1;
+//     });
+// }
+
+/**
+ * @function
+ * @description Get timezones in which day is changed on the given utc date time
+ * @returns {string[]}
+ */
+// export function getDayChangedTimezones(dateTime: dayjs.Moment): string[] {
+//     let tzs: string[] = [];
+
+//     return getAvailableTimezones().filter((tz) => {
+//         let dt: dayjs.Moment = dateTime.clone();
+//         dt.tz(tz).set('seconds', 0);
+//         return dt.format('HH:mm:ss') == '00:00:00';
+//     });
+// }
+/**
+ * @function
+ * @description Get timezones in which week is changed on the given utc date time
+ * @returns {string[]}
+ */
+// export function getWeekChangedTimezones(dateTime: dayjs.Moment): string[] {
+//     let tzs: string[] = [];
+
+//     return getDayChangedTimezones(dateTime).filter((tz) => {
+//         let dt: dayjs.Moment = dateTime.clone();
+//         dt.tz(tz).set('seconds', 0);
+//         let dt2 = dt.clone();
+//         dt2.subtract('second', 1);
+//         return dt.format(AllowDateFormat.YearWeek) != dt2.format(AllowDateFormat.YearWeek);
+//     });
+// }
+/**
+ * @function
+ * @description Get timezones in which month is changed on the given utc date time
+ * @returns {string[]}
+ */
+// export function getMonthChangedTimezones(dateTime: dayjs.Moment): string[] {
+//     let tzs: string[] = [];
+
+//     return getDayChangedTimezones(dateTime).filter((tz) => {
+//         let dt: dayjs.Moment = dateTime.clone();
+//         dt.tz(tz).set('seconds', 0);
+//         let dt2 = dt.clone();
+//         dt2.subtract('seconds', 1);
+//         return dt.format(AllowDateFormat.YearMonth) != dt2.format(AllowDateFormat.YearMonth);
+//     });
+// }
+
+export function ConvertMillisecondsToHour(time: number) {
+  return (time / (1000 * 60 * 60)) % 24;
+}
