51e8a2fe989aa37d11ca05c288077049aad6fa59
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-05-09 17:24:27 +0500

    changed smtp credentials and updated auth

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,7 +1,6 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { MAIL_ENV } from './mailconstant';
 import { ForgotPasswordDto } from '../app/auth/dto';
 import { Status, UserType } from '@prisma/client';
 
@@ -9,23 +8,21 @@ import { Status, UserType } from '@prisma/client';
 export class MailService {
   constructor(
     private mailerService: MailerService,
-    private configService: ConfigService,
+    private config: ConfigService,
   ) {}
 
   async sendUserInvitation(user) {
     try {
       await this.mailerService.sendMail({
         to: user.email,
-        from: MAIL_ENV.MAIL_FROM,
-        subject: `${this.configService.get(
-          'APP_NAME',
-        )} - Complete Your Registration`,
+        from: this.config.get('MAIL_FROM'),
+        subject: `${this.config.get('APP_NAME')} - Complete Your Registration`,
         template: 'user-invitation', // `.hbs` extension is appended automatically
         context: {
-          app_name: this.configService.get('APP_NAME'),
+          app_name: this.config.get('APP_NAME'),
           register_hash: user.register_hash,
-          app_url: this.configService.get('APP_URL') || process.env.APP_URL,
-          copyright_year: MAIL_ENV.COPYRIGHT_YEAR,
+          app_url: this.config.get('APP_URL') || process.env.APP_URL,
+          copyright_year: this.config.get('COPYRIGHT_YEAR'),
         },
       });
     } catch (error) {
@@ -40,16 +37,16 @@ export class MailService {
     try {
       await this.mailerService.sendMail({
         to: resetPasswordDataDto.email,
-        from: this.configService.get('MAIL_FROM'),
-        subject: `${this.configService.get('APP_NAME')} - Reset Your Password`,
+        from: this.config.get('MAIL_FROM'),
+        subject: `${this.config.get('APP_NAME')} - Reset Your Password`,
         template: 'resetPassword', // `.hbs` extension is appended automatically
         context: {
-          app_name: this.configService.get('APP_NAME'),
-          app_url: this.configService.get('APP_URL'),
-          copyright_year: MAIL_ENV.COPYRIGHT_YEAR,
+          app_name: this.config.get('APP_NAME'),
+          app_url: this.config.get('APP_URL'),
+          copyright_year: this.config.get('COPYRIGHT_YEAR'),
           otp,
           //   reset_password_link: `${
-          //     this.configService.get('APP_URL') || process.env.APP_URL
+          //     this.config.get('APP_URL') || process.env.APP_URL
           //   }/reset/${user_row.id}/${user_row.register_hash}`,
         },
       });
@@ -64,15 +61,15 @@ export class MailService {
     try {
       await this.mailerService.sendMail({
         to: email,
-        // from: this.configService.get('MAIL_FROM'),
+        from: this.config.get('MAIL_FROM'),
         subject: `${
-          this.configService.get('APP_NAME') || process.env.APP_NAME
+          this.config.get('APP_NAME') || process.env.APP_NAME
         } - Password Reset Confirmation`,
         template: 'confirm-password', // `.hbs` extension is appended automatically
         context: {
-          app_name: this.configService.get('APP_NAME') || process.env.APP_NAME,
-          app_url: this.configService.get('APP_URL') || process.env.APP_URL,
-          copyright_year: MAIL_ENV.COPYRIGHT_YEAR,
+          app_name: this.config.get('APP_NAME') || process.env.APP_NAME,
+          app_url: this.config.get('APP_URL') || process.env.APP_URL,
+          copyright_year: this.config.get('COPYRIGHT_YEAR'),
         },
       });
     } catch (error) {
@@ -84,20 +81,20 @@ export class MailService {
     try {
       await this.mailerService.sendMail({
         to: user.companyEmail,
-        from: MAIL_ENV.MAIL_FROM,
-        subject: `${this.configService.get(
+        from: this.config.get('MAIL_FROM'),
+        subject: `${this.config.get(
           'APP_NAME',
         )} - Vendor ${user.status.toLowerCase()}`,
         template: 'vendorApprovedRejected', // `.hbs` extension is appended automatically
         context: {
-          app_name: this.configService.get('APP_NAME'),
-          app_url: `${this.configService.get('APP_URL')}`,
+          app_name: this.config.get('APP_NAME'),
+          app_url: `${this.config.get('APP_URL')}`,
           first_name: user.fullName,
           message:
             user.status === Status.APPROVED
               ? 'Your account has been approved. You can now log in and start your journey with us!'
               : 'Your account has been rejected. Please contact our support for further information.',
-          copyright_year: MAIL_ENV.COPYRIGHT_YEAR,
+          copyright_year: this.config.get('COPYRIGHT_YEAR'),
         },
       });
     } catch (error) {
@@ -111,19 +108,17 @@ export class MailService {
       await this.mailerService.sendMail({
         // to: 'sheraznabimirza@gmail.com',
         to: user.email,
-        from: MAIL_ENV.MAIL_FROM,
-        subject: `${this.configService.get('APP_NAME')} - ${
-          user.userType
-        } created`,
+        from: this.config.get('MAIL_FROM'),
+        subject: `${this.config.get('APP_NAME')} - Account has been created`,
         template: 'vendorApprovedRejected', // `.hbs` extension is appended automatically
         context: {
-          app_name: this.configService.get('APP_NAME'),
-          app_url: `${this.configService.get('APP_URL')}`,
-          first_name: user.fullName,
+          app_name: this.config.get('APP_NAME'),
+          app_url: `${this.config.get('APP_URL')}`,
+          first_name: user[user.userType.toLowerCase()].fullName,
           message: `${`${
             user[user.userType.toLowerCase()].fullName
           } has signed up and waiting for approval.`}`,
-          copyright_year: MAIL_ENV.COPYRIGHT_YEAR,
+          copyright_year: this.config.get('COPYRIGHT_YEAR'),
         },
       });
     } catch (error) {
@@ -141,18 +136,16 @@ export class MailService {
       console.log('working');
       await this.mailerService.sendMail({
         to: user.email,
-        from: MAIL_ENV.MAIL_FROM,
-        subject: `${this.configService.get(
-          'APP_NAME',
-        )} - Registration Complete`,
+        from: this.config.get('MAIL_FROM'),
+        subject: `${this.config.get('APP_NAME')} - Registration Complete`,
         template: 'userRegistration', // `.hbs` extension is appended automatically
         context: {
-          app_name: this.configService.get('APP_NAME'),
-          app_url: `${this.configService.get(
+          app_name: this.config.get('APP_NAME'),
+          app_url: `${this.config.get(
             'APP_URL',
           )}/auth/verify-email/${encrypted}`,
           first_name: user[userType.toLowerCase()].fullName,
-          copyright_year: MAIL_ENV.COPYRIGHT_YEAR,
+          copyright_year: this.config.get('COPYRIGHT_YEAR'),
         },
       });
     } catch (error) {
