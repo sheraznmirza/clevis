0dd4b64cfaa8ffd02f34338bf965d291941d1ae9
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-05-17 21:14:17 +0500

    added media service import fixes

--- src/modules/app/media/media.module.ts
+++ src/modules/app/media/media.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 // import DatabaseModule from 'src/database/database.module';
 // import MediaController from './media.controller';
 import { MediaController } from './media.controller';
-import MediaService from './media.service';
-// import S3Service from './s3.service';
-
+import { MediaService } from './media.service';
 @Module({
   //   imports: [DatabaseModule],
   exports: [MediaService],
