6be24c86d3329bda1aed7ee40520c02f71937505
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-05-19 22:33:52 +0500

    commented umairs code

--- src/modules/app/ratingSetup/ratingSetup.controller.ts
+++ src/modules/app/ratingSetup/ratingSetup.controller.ts
@@ -1,49 +1,49 @@
-import {
-  Controller,
-  Post,
-  Get,
-  Param,
-  Patch,
-  Delete,
-  Body,
-} from '@nestjs/common';
-import { Authorized } from 'src/core/decorators';
-import { UserType } from '@prisma/client';
-import { RatingSetup } from './ratingsetup.service';
-import { RatingSetupDto } from './dto/ratingsetup.dto';
-import { ApiTags } from '@nestjs/swagger';
+// import {
+//   Controller,
+//   Post,
+//   Get,
+//   Param,
+//   Patch,
+//   Delete,
+//   Body,
+// } from '@nestjs/common';
+// import { Authorized } from 'src/core/decorators';
+// import { UserType } from '@prisma/client';
+// import { RatingSetup } from './ratingsetup.service';
+// import { RatingSetupDto } from './dto/ratingsetup.dto';
+// import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('Rating')
-@Controller('rating')
-export class RatingSetupController {
-  constructor(private ratingSetup: RatingSetup) {}
+// @ApiTags('Rating')
+// @Controller('rating')
+// export class RatingSetupController {
+//   constructor(private ratingSetup: RatingSetup) {}
 
-  @Authorized([UserType.ADMIN])
-  @Get('/:ratingId')
-  getRatingById(@Param('ratingId') id: number) {
-    return this.ratingSetup.getRatingById(id);
-  }
+//   @Authorized([UserType.ADMIN])
+//   @Get('/:ratingId')
+//   getRatingById(@Param('ratingId') id: number) {
+//     return this.ratingSetup.getRatingById(id);
+//   }
 
-  @Authorized(UserType.ADMIN)
-  @Post()
-  createRating(@Body() data: RatingSetupDto) {
-    return this.ratingSetup.createRating(data);
-  }
+//   @Authorized(UserType.ADMIN)
+//   @Post()
+//   createRating(@Body() data: RatingSetupDto) {
+//     return this.ratingSetup.createRating(data);
+//   }
 
-  @Authorized(UserType.ADMIN)
-  @Patch('/:ratingId')
-  updateRating(@Param('ratingId') id: number, @Body() data: RatingSetupDto) {
-    return this.ratingSetup.updateRating(id, data);
-  }
+//   @Authorized(UserType.ADMIN)
+//   @Patch('/:ratingId')
+//   updateRating(@Param('ratingId') id: number, @Body() data: RatingSetupDto) {
+//     return this.ratingSetup.updateRating(id, data);
+//   }
 
-  @Delete('/:ratingId')
-  deleteRating(@Param('ratingId') id: number) {
-    return this.ratingSetup.deleteRating(id);
-  }
+//   @Delete('/:ratingId')
+//   deleteRating(@Param('ratingId') id: number) {
+//     return this.ratingSetup.deleteRating(id);
+//   }
 
-  @Authorized(UserType.ADMIN)
-  @Get()
-  getAllRating() {
-    return this.ratingSetup.getAllRating();
-  }
-}
+//   @Authorized(UserType.ADMIN)
+//   @Get()
+//   getAllRating() {
+//     return this.ratingSetup.getAllRating();
+//   }
+// }
