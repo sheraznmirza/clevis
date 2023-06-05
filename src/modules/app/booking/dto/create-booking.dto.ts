001683852823d8e904afd07f69acc0f59643f789
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-06-05 19:33:34 +0500

    datestring

--- src/modules/app/booking/dto/create-booking.dto.ts
+++ src/modules/app/booking/dto/create-booking.dto.ts
@@ -50,13 +50,11 @@ class LocationType {
   longitude?: number;
 
   @ApiProperty()
-  // @IsString()
   @IsDateString()
   @IsNotEmpty()
   timeFrom: string;
 
   @ApiProperty()
-  // @IsString()
   @IsDateString()
   @IsNotEmpty()
   timeTill: string;
@@ -64,7 +62,7 @@ class LocationType {
 
 export class CreateBookingDto {
   @ApiProperty()
-  @IsString()
+  @IsDateString()
   @IsNotEmpty()
   bookingDate: string;
 
