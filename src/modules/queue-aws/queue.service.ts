9a2be4ff61cb39d5d29c9495c716a01eeb85c0d4
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-06-06 21:04:29 +0500

    changed names quueue

--- src/modules/queue-aws/queue.service.ts
+++ src/modules/queue-aws/queue.service.ts
@@ -8,7 +8,7 @@ import {
   SQSSendEmailArgs,
   SQSSendNotificationArgs,
 } from './types';
-import { NotificationDataT } from '../notification/types';
+import { NotificationDataT } from '../notification-socket/types';
 
 @Injectable()
 export default class QueueService {
