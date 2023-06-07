f571ca7bfda55926b8003dae0cbe48b2fe4e6a1b
umair saleem <umair.saleem@koderlabs.com>
2023-06-07 21:10:11 +0500

    Create Booking Carwash/laundry APi

--- src/modules/app/booking/dto/index.ts
+++ src/modules/app/booking/dto/index.ts
@@ -2,3 +2,4 @@ export * from './create-booking.dto';
 export * from './get-bookings.dto';
 export * from './update-booking-status.dto';
 export * from './booking-details.dto';
+export * from './create-booking-carwash.dto';
