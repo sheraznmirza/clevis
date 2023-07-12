0f185e0d00f8136bf8886f70534263ab8a7a8d7e
Sheraz Nabi Mirza <sheraz.nabi@koderlabs.com>
2023-07-12 19:47:57 +0500

    platform fee limit

--- src/modules/app/platformSetup/dto/platform-setup.dto.ts
+++ src/modules/app/platformSetup/dto/platform-setup.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, Max, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PlatFormSetupDto {
   @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
+  @Max(10000)
+  @Min(1)
   fee: number;
 }
